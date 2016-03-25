d450a8dec8431614b6a4742b56182a3938b5b50d
wangwei1 <wangwei1@xiaomi.com>
2016-03-25 15:51:15 +0800

    change block focus policy

--- tvhome/app/src/main/java/com/mothership/tvhome/widget/DebugVerticalGridView.java
+++ tvhome/app/src/main/java/com/mothership/tvhome/widget/DebugVerticalGridView.java
@@ -1,15 +1,18 @@
 package com.mothership.tvhome.widget;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.os.Parcelable;
 import android.support.v17.leanback.widget.VerticalGridView;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 
 /**
  * Created by wangwei on 3/22/16.
  */
 public class DebugVerticalGridView extends VerticalGridView {
-    final String TAG = "fps";
+    final String TAG = "GridView";
 
     public DebugVerticalGridView(Context context) {
         this(context, null);
@@ -40,15 +43,76 @@ public class DebugVerticalGridView extends VerticalGridView {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        Log.d(TAG,"onLayout 0");
+        Log.d(TAG, "onLayout 0");
         super.onLayout(changed, l, t, r, b);
         //Log.d(TAG, "onLayout 1",new Exception(""));
     }
 
     @Override
     public void scrollBy(int x, int y) {
-        Log.d(TAG,"scrollBy 0");
+        Log.d(TAG, "scrollBy 0");
         super.scrollBy(x, y);
         //Log.d(TAG, "scrollBy 1", new Exception(""));
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        return super.onSaveInstanceState();
+    }
+
+    @Override
+    public boolean onRequestFocusInDescendants(int direction, Rect previouslyFocusedRect) {
+        Log.d(TAG,"onRequestFocusInDescendants",new Exception(""));
+
+        int index;
+        int increment;
+        int end;
+        int count = getChildCount();
+        if((direction & FOCUS_UP) != 0) {
+            Rect better = new Rect();
+            getChildAt(0).getFocusedRect(better);
+            this.offsetDescendantRectToMyCoords(getChildAt(0), better);
+            int pos = 0;
+            for (int i = 1; i != count; i += 1) {
+                View child = getChildAt(i);
+                if (child.getVisibility() == VISIBLE) {
+                    Rect rect = new Rect();
+                    getChildAt(i).getFocusedRect(rect);
+                    this.offsetDescendantRectToMyCoords(getChildAt(i), rect);
+                    if(rect.bottom>better.bottom){
+                        better = rect;
+                        pos = i;
+                    }
+                }
+            }
+            if(pos<count){
+                if (getChildAt(pos).requestFocus(direction, previouslyFocusedRect)) {
+                    return true;
+                }
+            }
+
+        }else{
+            if ((direction & FOCUS_FORWARD) != 0) {
+                index = 0;
+                increment = 1;
+                end = count;
+
+            } else {
+                index = count - 1;
+                increment = -1;
+                end = -1;
+            }
+
+            for (int i = index; i != end; i += increment) {
+                View child = getChildAt(i);
+                if (child.getVisibility() == VISIBLE) {
+                    if (child.requestFocus(direction, previouslyFocusedRect)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
--- tvhome/app/src/main/java/com/mothership/tvhome/widget/FocusVerticalGridView.java
+++ tvhome/app/src/main/java/com/mothership/tvhome/widget/FocusVerticalGridView.java
@@ -2,68 +2,26 @@ package com.mothership.tvhome.widget;
 
 import android.content.Context;
 import android.graphics.Rect;
-import android.os.Parcelable;
-import android.support.v7.widget.GridLayout;
+import android.support.v17.leanback.widget.VerticalGridView;
 import android.util.AttributeSet;
 import android.view.View;
 
-import java.util.ArrayList;
-
 /**
- * Created by wangwei on 3/15/16.
+ * Created by wangwei on 3/22/16.
  */
-public class FocusGridLayout extends GridLayout {
-    public FocusGridLayout(Context context) {
-        this(context, null, 0);
+public class FocusVerticalGridView extends VerticalGridView {
+    final String TAG = "FocusVerticalGridView";
+
+    public FocusVerticalGridView(Context context) {
+        this(context, null);
     }
 
-    public FocusGridLayout(Context context, AttributeSet attrs) {
+    public FocusVerticalGridView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public FocusGridLayout(Context context, AttributeSet attrs, int defStyle) {
+    public FocusVerticalGridView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setChildrenDrawingOrderEnabled(true);
-        setFocusable(true);
-        setFocusableInTouchMode(true);
-        setDescendantFocusability(FOCUS_AFTER_DESCENDANTS);
-    }
-    @Override
-    public void requestChildFocus(View child, View focused)
-    {
-        super.requestChildFocus(child, focused);
-        invalidate();
-    }
-
-    @Override
-    protected int getChildDrawingOrder(int childCount, int i)
-    {
-        View focus = getFocusedChild();
-        if(null == focus)
-        {
-            return i;
-        }
-        else
-        {
-            int focusIdx = indexOfChild(focus);
-            if(i < focusIdx)
-            {
-                return  i;
-            }
-            else if(i < childCount - 1)
-            {
-                return focusIdx + childCount - 1 - i;
-            }
-            else
-            {
-                return focusIdx;
-            }
-        }
-    }
-
-    @Override
-    protected Parcelable onSaveInstanceState() {
-        return super.onSaveInstanceState();
     }
 
     @Override
@@ -119,17 +77,5 @@ public class FocusGridLayout extends GridLayout {
         }
 
         return false;
-    }
-
-    @Override
-    public void addFocusables(ArrayList<View> views, int direction, int focusableMode) {
-        if(hasFocus()){
-            super.addFocusables(views,direction,focusableMode);
-        }else{
-            if (isFocusable()) {
-                views.add(this);
-            }
-        }
-        return ;
     }
 }
